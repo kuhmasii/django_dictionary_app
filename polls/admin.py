0276cd03e258484141e1f28ad5f32ac98d9ca02d
isaiah olaoye <olaisaiah54@gmail.com>
2021-11-11 00:50:40 +0100

    Made changes in template files, views and settings

--- polls/admin.py
+++ polls/admin.py
@@ -2,16 +2,23 @@ from django.contrib import admin
 from . models import Choice, Question
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = "question_asked pub_date".split()
+    list_display = "question_asked pub_date was_published_recently".split()
     list_filter = "pub_date".split()
     # readonly_fields = ("pub_date",)
-
-
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = "choice_asked votes".split()
-    list_filter = "votes".split()
+    search_fields = ["question_asked"]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice, ChoiceAdmin)
+
+
+# class ChoiceAdmin(admin.ModelAdmin):
+#     list_display = "choice_asked votes".split()
+#     list_filter = "votes".split()
+# admin.site.register(Choice, ChoiceAdmin)
--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.utils import timezone
 from . models import Question, Choice
 from django.urls import reverse
 
 
 def index(request):
     # fetching out first five latest question from database
-    latest_question_list = Question.objects.order_by("pub_date")[:5]
+    latest_question_list = Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by("-pub_date")[:5]
 
     return render(request, 'polls/index.html', dict(latest_question_list=latest_question_list))
 
 
 def detail(request, question_id):
     try:
-        question = Question.objects.get(pk=question_id)
-        choice = question.choices.all()
+        question = Question.objects.get(
+            pk=question_id, 
+            pub_date__lte=timezone.now()
+        )
+        choice = question.choices.order_by("-id")
         # this is another way to query the choices asked when not using the argument related_names
         # choice = Choice.objects.all()
         # choice = Choice.objects.filter(question=question)
@@ -26,7 +32,7 @@ def detail(request, question_id):
 def results(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
-        choice = question.choices.all()
+        choice = question.choices.order_by("-id")
     except Question.DoesNotExist:
         raise Http404("Query or Question you are looking for not in database")
     return render(request, 'polls/results.html', dict(question=question, choice=choice))
